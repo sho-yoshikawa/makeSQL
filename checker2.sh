d6c321eea88ca1a69f29a0c73c036842cc4f8738
syoshika <syoshika@student.42tokyo.jp>
2024-01-12 09:02:15 +0900

    add more separator

--- checker2.sh
+++ checker2.sh
@@ -1,8 +1,15 @@
 SQL_FILE=$1
 ORIGIN_FILE=$2
 
+# single quote 「'」
 cat $SQL_FILE | grep "\'" > a
 cat a | tr -d '\,' > b
 cat b | tr -d "\'" > c
 
+# double quote 「"」
+#cat $SQL_FILE | grep '\"' > a
+#cat a | tr -d '\,' > b
+#cat b | tr -d '\"' > c
+
 diff c $ORIGIN_FILE
+rm a b c
